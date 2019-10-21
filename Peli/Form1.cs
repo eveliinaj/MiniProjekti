300f8f845e5bf02683c67d6f70106af77bca9da6
Siiri Helin <siiri.lydia@hotmail.com>
2019-10-21 18:24:07 +0300

    Add project files.

---
+++ Peli/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Lemmikki lemmikki = new Lemmikki();
+
+            string[] taulukko = lemmikki.NäytäEläin();
+
+            //string unicodeString = char.ConvertFromUtf32(0x1F967);
+
+            //textBox1.Text = unicodeString;
+
+            foreach (var rivi in taulukko)
+            {
+                textBox1.Text += rivi + Environment.NewLine;
+            }
+
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Peli/Lemmikki.cs
@@ -0,0 +1,19 @@
+﻿namespace Peli
+{
+    internal class Lemmikki
+    {
+        public Lemmikki()
+        {
+
+        }
+
+        public string[] NäytäEläin()
+        {
+
+            string [] kuvio = { " _________ ", "|         |", "|WE HATE U|", "|_________|", "(\\__/)||   ", "( >< ) ||    ", "( oo )||" };
+
+            return kuvio;
+        }
+
+    }
+}
